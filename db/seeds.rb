8b0d378c7fcb3cb1e93566ec999da854e9972a78
scottms724 <scottms724@gmail.com>
2020-12-06 12:52:57 -0500

    user backend built sans serializer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+User.create(name: "scottms724", username: "scottms724@gmail.com", img_url: "https://st.depositphotos.com/1001911/1222/v/600/depositphotos_12221489-stock-illustration-big-smile-emoticon.jpg", password: "pass123")
+User.create(name: "adamds", username: "adamds1224@gmail.com", img_url: "https://previews.123rf.com/images/vectorkif/vectorkif1802/vectorkif180200028/94910807-business-man-with-blond-hair-cartoon-character-handsome-businessman-in-suit-waving-hand-vector-illus.jpg", password: "pass123")
