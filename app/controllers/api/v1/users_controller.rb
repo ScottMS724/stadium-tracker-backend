3b941dc4949905616da03763e618eaaa8f734edd
scottms724 <scottms724@gmail.com>
2020-12-26 13:12:59 -0500

    add user#create functionality

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,9 +16,13 @@ class Api::V1::UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-          render json: @user, status: :created, location: @user
+          session[:user_id] = @user.id 
+          render json: UserSerializer.new(@user), status: :created
         else
-          render json: @user.errors, status: :unprocessable_entity
+          resp = {
+            error: @user.errors.full_messages.to_sentence
+          }
+          render json: resp, status: :unprocessable_entity
         end
       end
 
@@ -41,6 +45,6 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def user_params
-        params.require(:user).permit(:name, :username, :img_url, :email)
+        params.require(:user).permit(:name, :username, :password, :img_url)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     has_many :stadiums
     has_many :visits, through: :stadiums 
     validates :username, uniqueness: true
+    validates :name, :username, :img_url, presence: true 
 end
