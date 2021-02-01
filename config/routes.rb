01d6efcfa79786c8cc0a25c5ae284b4c54b1d0be
scottms724 <scottms724@gmail.com>
2021-02-01 11:21:19 -0500

    add nested visits routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       resources :users 
-      resources :stadiums
-      resources :visits
+      resources :visits, only: [:index]
+      resources :stadiums do
+        resources :visits
+      end
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
