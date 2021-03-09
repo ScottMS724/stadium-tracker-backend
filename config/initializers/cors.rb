37e5a2a28cefff6584e949096b7b140ec8bb5bb2
scottms724 <scottms724@gmail.com>
2021-03-09 13:37:09 -0500

    cors origins Heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://young-inlet-18987.herokuapp.com/'
 
     resource '*',
       headers: :any,
