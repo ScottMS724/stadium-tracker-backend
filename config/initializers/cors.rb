bb7a993ebb576ebb68bda90d0ec06203caa19f16
scottms724 <scottms724@gmail.com>
2021-03-09 13:40:06 -0500

    cors origins change to Netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://young-inlet-18987.herokuapp.com/'
+    origins 'https://stadium-tracker.netlify.app'
 
     resource '*',
       headers: :any,
