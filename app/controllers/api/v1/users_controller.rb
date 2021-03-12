9699855a74b104ea2fd6e3d9bdfe560d3002da7e
scottms724 <scottms724@gmail.com>
2021-03-12 13:35:19 -0500

    new to create in userscontroller#create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(user_params)
+        @user = User.create(user_params)
 
         if @user.save
           session[:user_id] = @user.id 
